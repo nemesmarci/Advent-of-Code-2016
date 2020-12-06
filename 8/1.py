fb7481a485706c90929965aeb34e68abdd2d6523
Marcell Nemes <nemes@sch.bme.hu>
2020-12-06 18:42:03 +0100

    Day 8: Two-Factor Authentication

---
+++ 8/1.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+from common import run
+
+print(np.sum(run()))
---
+++ 8/2.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+from common import run
+
+plt.axis('off')
+plt.imshow(run(), cmap='Greys')
+plt.show()
